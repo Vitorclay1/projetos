2aa3805b2047882182eacf8fb7942e7d02583843
nti <vitorclay12@gmail.com>
2024-10-25 14:15:19 -0300

    adicionado metodos de post e get

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
+tasks = []
+task_id_control = 1
+@app.route('/tasks', methods=['POST'])
+def create_tasks():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ''))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({'message': 'Nova tarefa criada com sucesso'})
 
-@app.route('/')
-def home():
-    """
-    This function is a simple Flask route that returns a welcome message.
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
 
-    Parameters:
-    None
+    output = {
+        "tasks": task_list,
+        "total_tasks": 0
+    }
 
-    Returns:
-    str: A string containing the welcome message 'Hello, World!'
-    """
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'pagina sobre'
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
